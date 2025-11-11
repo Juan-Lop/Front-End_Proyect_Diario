2b021abc75818ace8e449e994f341dae5d1a9d8d
Juan-Lop <juan.munozlop@campusucc.edu.co>
2025-11-11 17:32:26 -0500

    docs: agregar documentación al módulo de utilidades

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
+/**
+ * Utilidades generales
+ * Funciones auxiliares para uso en toda la aplicación
+ */
+
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+/**
+ * Combina clases de Tailwind CSS de manera óptima
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
